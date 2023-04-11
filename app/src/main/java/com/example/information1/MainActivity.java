478340f4f945e81ab3c4bdb4371a4104f497702e
JS-404-000 <hondarr@naver.com>
2023-04-11 15:23:40 +0900

    commit 2 --> 메인액티비티일부 구현

--- app/src/main/java/com/example/information1/MainActivity.java
+++ app/src/main/java/com/example/information1/MainActivity.java
@@ -6,15 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tVResult, tVResultLabel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnReq = findViewById(R.id.btnRequest);
         Button btnEnd = findViewById(R.id.btnEnd);
+        tVResult = findViewById(R.id.tVResult);
+        tVResultLabel = findViewById(R.id.tVResultLabel);
+        EditText editTextID = findViewById(R.id.editTextID);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
