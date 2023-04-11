bde11d70dbc0c0b0ca921b13d2dd417113a36d5d
JS-404-000 <hondarr@naver.com>
2023-04-11 15:13:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/information1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.information1;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnReq = findViewById(R.id.btnRequest);
+        Button btnEnd = findViewById(R.id.btnEnd);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
